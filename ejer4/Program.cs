91667861d14ee91827ceddaac6c0816a7cf1d59c
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-09 11:30:36 -0300

    lee numeros

---
+++ ejer4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ejer4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1, num2;
+
+            Console.WriteLine("Ingrese el primer numero");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("Ingrese el segundo numero");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();
+
+
+
+        
+        
+        }
+    }
+}
